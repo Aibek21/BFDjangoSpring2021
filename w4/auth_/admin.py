f8bc43f3978c860ef09ce7db5aa8abf9d1037d20
Aibek21 <aibekkuralbaev@gmail.com>
2021-03-05 21:15:22 +0600

    Custom auth

---
+++ w4/auth_/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+# from auth_.models import Profile
+from auth_.models import MainUser
+
+# Register your models here.
+# admin.site.register(Profile)
+admin.site.register(MainUser)
